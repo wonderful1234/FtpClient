26de4831c644d975d9dd2425530e0629fc32dd01
wonderful1234 <1763326347@qq.com>
2022-07-16 01:13:36 +0800

    adapt dpi

--- FtpClient/src/main.cpp
+++ FtpClient/src/main.cpp
@@ -3,7 +3,10 @@
 #include "Log.h"
 int main(int argc, char *argv[])
 {
-	
+
+	QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+	QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication a(argc, argv);
 	Log::getInstance();
     FtpClient w;
