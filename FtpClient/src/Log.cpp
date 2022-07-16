e2c01cc3219c78a15d3b54c76b281827443096b5
wonderful1234 <1763326347@qq.com>
2022-07-16 21:22:13 +0800

    fix log

--- FtpClient/src/Log.cpp
+++ FtpClient/src/Log.cpp
@@ -7,6 +7,8 @@ Log::Log()
 {
 	m_logPath = QCoreApplication::applicationDirPath() + "/FtpLog";
 	m_logger = spdlog::basic_logger_mt("basic_logger", m_logPath.toLocal8Bit().data());
+	m_logger->set_level(spdlog::level::info);
+	m_logger->flush_on(spdlog::level::info);
 	m_logger->set_pattern("%Y-%m-%d %H:%M:%S.%e %l  %v......");
 }
 
@@ -25,21 +27,18 @@ void Log::logInfo(const QString & info)
 {
 	std::string log = info.toLocal8Bit().data();
 	m_logger->info(log);
-	m_logger->flush();
 }
 
 void Log::logError(const QString & error)
 {
 	std::string log = error.toLocal8Bit().data();
 	m_logger->error(log);
-	m_logger->flush();
 }
 
 void Log::logWarn(const QString & warn)
 {
 	std::string log = warn.toLocal8Bit().data();
 	m_logger->warn(log);
-	m_logger->flush();
 }
 
 void Log::closeLog()
